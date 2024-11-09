69a1e2bcf2aeff30474f5d002c2f750bf78c5517
Mary Jonah <jonahmary17@gmail.com>
2024-11-09 13:11:53 +0000

    Node.js and Express Exercise 3.4

--- index.js
+++ index.js
@@ -43,6 +43,12 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const personId = request.params.id 
+    persons = persons.filter(person => person.id != personId)
+    response.status(204).end()
+})
+
 const PORT=3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
